1aa180e139f312cd7b700e590c2b12f0ca0f5881
RajarshiBandhu <97370250+RajarshiBandhu@users.noreply.github.com>
2024-10-15 14:56:07 -0500

    pid done

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -70,7 +70,7 @@ public class MecanumDrive {
         // feedforward parameters (in tick units)
         public double kS = 0.9909361569145583;
         public double kV = 0.13559321618732706;
-        public double kA = 0;
+        public double kA = 0.023;
 
         // path profile parameters (in inches)
         public double maxWheelVel = 50;
@@ -82,9 +82,9 @@ public class MecanumDrive {
         public double maxAngAccel = Math.PI;
 
         // path controller gains
-        public double axialGain = 0.0;
-        public double lateralGain = 0.0;
-        public double headingGain = 0.0; // shared with turn
+        public double axialGain = 4;
+        public double lateralGain = 3.0;
+        public double headingGain = 3.2; // shared with turn
 
         public double axialVelGain = 0.0;
         public double lateralVelGain = 0.0;
