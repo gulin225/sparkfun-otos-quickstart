9d4a07fc0f77d371d6df730dea8ef1da09611bb1
gulin225 <123128271+gulin225@users.noreply.github.com>
2024-10-16 15:41:39 -0500

    PID teleop not tested

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/VerticalSlides.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/VerticalSlides.java
@@ -2,28 +2,115 @@ package org.firstinspires.ftc.teamcode.Subsystems;
 
 import android.sax.StartElementListener;
 
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.arcrobotics.ftclib.controller.PIDController;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class VerticalSlides {
-    DcMotorEx backLeftSlide, backRightSlide, frontLeftSlide, frontRightSlide;
+    private PIDController controllerUp, controllerDown;
+
+
+    boolean leftBackFrontRight = true;
+    public static double f = 0.1;
+
+   // public static int target =0;
+    //p = 0.01 d = .001
+    public static double pUp = 0.021, iUp = 0, dUp = 0.000225;
+    public static double pDown = 0.015, iDown = 0, dDown = 0.00012;
+    public final double ticks_in_degrees = 145.1/360; //num of ticks per rotation we need to find this out
+    DcMotorEx backLeftSlide, backRightSlidePID, frontLeftSlide, frontRightSlide;
+
 
     public VerticalSlides(HardwareMap hardwareMap){
+        controllerUp = new PIDController(pUp,iUp,dUp);
+        controllerDown = new PIDController(pDown, iDown, dDown);
+
+
         backLeftSlide = hardwareMap.get(DcMotorEx.class,"leftBackS");
-        backRightSlide = hardwareMap.get(DcMotorEx.class,"rightBackS");
+        backRightSlidePID = hardwareMap.get(DcMotorEx.class,"rightBackS");
         frontLeftSlide = hardwareMap.get(DcMotorEx.class,"leftFrontS");
         frontRightSlide = hardwareMap.get(DcMotorEx.class,"rightFrontS");
-        frontLeftSlide.setDirection(DcMotorSimple.Direction.REVERSE);
-        backRightSlide.setDirection(DcMotorSimple.Direction.REVERSE);
 
-        backRightSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        backRightSlidePID.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRightSlidePID.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+
+
+        frontLeftSlide.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRightSlidePID.setDirection(DcMotorSimple.Direction.REVERSE);
+
+    }
+
+
+    public void PIDLoop(int target){
+
+        int armPos = backRightSlidePID.getCurrentPosition();
+
+        if(target > armPos) {
+            controllerUp.setPID(pUp,iUp,dUp);
+            double pid = controllerUp.calculate(armPos, target);
+
+            double ff = Math.cos(Math.toRadians(target / ticks_in_degrees)) * f;
+
+            double power = pid + ff;
+            if(power > 1) power = 1;
+            backRightSlidePID.setPower(power);
+            backLeftSlide.setPower(power);
+            frontLeftSlide.setPower(power);
+            frontRightSlide.setPower(power);
+
+        }
+        else{
+            controllerDown.setPID(pDown, iDown,dDown);
+            double pid = controllerDown.calculate(armPos, target);
+
+            double ff = Math.cos(Math.toRadians(target / ticks_in_degrees)) * f;
+
+            double power = pid + ff;
+
+            int num = 1;
+            if(num == 1){
+                backLeftSlide.setPower(power);
+                frontRightSlide.setPower(power);
+                frontLeftSlide.setPower(power);
+                num = 2;
+            } else if (num ==2) {
+                frontRightSlide.setPower(power);
+                frontLeftSlide.setPower(power);
+                backRightSlidePID.setPower(power);
+                num = 3;
+            } else if (num == 3) {
+                backLeftSlide.setPower(power);
+                frontRightSlide.setPower(power);
+                backRightSlidePID.setPower(power);
+                num =4;
+            } else if (num==4) {
+                backRightSlidePID.setPower(power);
+                backLeftSlide.setPower(power);
+                frontLeftSlide.setPower(power);
+                num = 1;
+            }
+
+
+        }
+    }
+
+    public void setSlidesUp(){
+        PIDLoop(1000);
+    }
+
+    public void setSlidesDown(){
+        PIDLoop(0);
     }
 
     public void test(double one, double two, double three, double four){
         backLeftSlide.setPower(one);
-        backRightSlide.setPower(two);
+        backRightSlidePID.setPower(two);
         frontLeftSlide.setPower(three);
         frontRightSlide.setPower(four);
 
@@ -33,11 +120,5 @@ public class VerticalSlides {
         backLeftSlide.setPower(.5);
     }
 
-    public double getEncoderValue(){
-        return backLeftSlide.getCurrentPosition();
-    }
 
-    public void PIDLoop(){
-
-    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TeleOperation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TeleOperation.java
@@ -31,10 +31,11 @@ public class TeleOperation extends LinearOpMode {
        robot.init();
 
        while (!isStopRequested() && opModeIsActive()){
-          // robot.verticalSlides.testBackLeft();
+
            if (gamepad2.left_bumper) robot.intake();
            if (gamepad2.right_bumper) robot.outtake();
-           if (gamepad2.dpad_down) robot.linearRail.moveRail(LinearRail.linearRailStates.middle);
+           if (gamepad2.dpad_down) robot.verticalSlides.setSlidesUp();
+           if(gamepad2.dpad_down) robot.verticalSlides.setSlidesDown();
             if(gamepad2.a) {
                 robot.claw.moveClaw(Claw.clawStates.intake);
                 robot.claw.moveClaw(Claw.clawStates.spinOn);
