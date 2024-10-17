6a8765e52199e1cc8528afff8b0dc0f59eafb198
RajarshiBandhu <97370250+RajarshiBandhu@users.noreply.github.com>
2024-10-16 20:43:40 -0500

    works

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutonomousTesting/ActionCreator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutonomousTesting/ActionCreator.java
@@ -1,4 +1,5 @@
-package org.firstinspires.ftc.teamcode.Autonomous.AutonomousTesting;
+
+        package org.firstinspires.ftc.teamcode.Autonomous.AutonomousTesting;
 
 import com.acmerobotics.roadrunner.ParallelAction;
 import com.acmerobotics.roadrunner.Pose2d;
@@ -23,10 +24,30 @@ public class ActionCreator {
                         .setTangent(Math.toRadians(0))
                         .splineToConstantHeading(new Vector2d(30,-10), Math.toRadians(0)).build()
                 );
+
+                intakeSample1 = new ParallelAction(drive.actionBuilder(new Pose2d(30,10,Math.toRadians(0)))
+                        .setTangent(Math.toRadians(0))
+                        .splineToLinearHeading(new Pose2d(48,10, Math.toRadians(90)), Math.toRadians(40)).build()
+                );
+
+                outtakeSample1 = new ParallelAction(drive.actionBuilder(new Pose2d(48,10,Math.toRadians(90)))
+                        .setTangent(Math.toRadians(180))
+                        .splineToLinearHeading(new Pose2d(16,32, Math.toRadians(135)), Math.toRadians(160)).build()
+                );
+
+                intakeSample2 = new ParallelAction(drive.actionBuilder(new Pose2d(16,32, Math.toRadians(135)))
+                        .setTangent(Math.toRadians(270))
+                        .splineToLinearHeading(new Pose2d(45,25, Math.toRadians(90)), Math.toRadians(60)).build()
+                );
+
+                outtakeSample2 = new ParallelAction(drive.actionBuilder(new Pose2d(45,25,Math.toRadians(90)))
+                        .setTangent(Math.toRadians(180))
+                        .splineToLinearHeading(new Pose2d(16,32, Math.toRadians(135)), Math.toRadians(160)).build()
+                );
                 break;
             case specimen:
                 break;
         }
-        return new SequentialAction(preload);
+        return new SequentialAction(preload, intakeSample1, outtakeSample1, intakeSample2, outtakeSample2, intakeSample2, outtakeSample2);
     }
 }
